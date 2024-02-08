9f60e78274737fe3560c5e08588e5d8acfd25a48
Ahmed Tarek Hagar <ahmed.tarek.ronaldo@gmail.com>
2024-02-08 10:40:15 +0200

    start framework

---
+++ src/app/section-line/section-line.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-section-line',
+  templateUrl: './section-line.component.html',
+  styleUrls: ['./section-line.component.css']
+})
+export class SectionLineComponent {
+}
